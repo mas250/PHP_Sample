a9244481cea650dcd2fa61ccb06c7ed58831d07e
Computer Setup <CSetup@spartaglobal.com>
2020-02-11 16:46:52 +0000

    simple form

--- form.php
+++ form.php
@@ -5,9 +5,31 @@ if(isset($_POST['searchterm'])) {
 ?>
 
 <form
-action=""
-method="post"
->
-<input type="text" name="searchterm">
-<input type="submit" value="Search">
+    action=""
+    method="post"
+    >
+    User name: <input type="text" name="name"><br>
+    Password: <input type="password" value="password"><br>
+    Gender:
+        <input type="radio" name ="gender" value="f"> Female
+        <input type="radio" name ="gender" value="m"> Male
+        <input type="radio" name ="gender" value="o"> Other<br>
+    Favorite color:
+    <select name="color" name="name">
+        <option value ="">Please select</option>
+        <option value ="#0f0">red</option>
+        <option value ="#00f">blue</option>
+        <option value ="#0f0">green</option>
+    </select><br>
+    Languuages Spoken:
+    <select name ="languages[]" multiple size="3">
+    <option value ="en">English</option>
+        <option value ="fr">French</option>
+        <option value ="it">Italian</option>
+    </select><br>
+    Comments: <textarea name="comments"></textarea><br>
+    <input type="checkbox" name ="tc" value="ok">
+    I accept the T&amp;C<br>
+    <input type="submit" name="Search"><br>
+
 </form>
