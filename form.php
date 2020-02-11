750239056e6e761abe756c372297e45202c8e0c6
Computer Setup <CSetup@spartaglobal.com>
2020-02-11 16:07:59 +0000

    inital commit

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['searchterm'])) {
+    echo htmlspecialchars($_POST['searchterm'], ENT_QUOTES);
+}
+?>
+
+<form
+action=""
+method="post"
+>
+<input type="text" name="searchterm">
+<input type="submit" value="Search">
+</form>
