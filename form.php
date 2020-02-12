739f2056bfd42e40ffa8aaa59ef9af2ffd704176
mas250 <mas250@exeter.ac.uk>
2020-02-12 11:49:19 +0000

    returning form data best practice

--- form.php
+++ form.php
@@ -1,6 +1,52 @@
 <?php
-if(isset($_POST['searchterm'])) {
-    echo htmlspecialchars($_POST['searchterm'], ENT_QUOTES);
+$name = '';
+$password = '';
+$gender = '';
+$color = '';
+$languages = [];
+$comments = '';
+$tc = '';
+
+if(isset($_POST['submit'])) {
+    
+    if(isset($_POST['name'])){
+        $name = $_POST['name'];
+    }
+    if(isset($_POST['$password'])){
+        $password = $_POST['$password'];
+    }
+    if(isset($_POST['gender'])){
+        $gender = $_POST['gender'];
+    }
+    if(isset($_POST['color'])){
+        $color = $_POST['color'];
+    }
+    if(isset($_POST['languages'])){
+        $languages = $_POST['languages'];
+    }
+    if(isset($_POST['comments'])){
+        $comments = $_POST['comments'];
+    }
+    if(isset($_POST['tc'])){
+        $tc = $_POST['tc'];
+    }
+
+    printf('User name : %s
+    <br>Password: %s
+    <br>Gender: %s
+    <br>Color: %s
+    <br>Languages: %s   
+    <br>Comments: %s
+    <br>T&amp;c: %s',
+    htmlspecialchars($names, ENT_QUOTES),       //better practice to use placeholders and
+    htmlspecialchars($password, ENT_QUOTES),    //escape special characters so users cant run malicious code
+    htmlspecialchars($gender, ENT_QUOTES),           
+    htmlspecialchars($color, ENT_QUOTES),           
+    htmlspecialchars(implode(' ', $languages), ENT_QUOTES),
+    htmlspecialchars($comments, ENT_QUOTES),
+    htmlspecialchars($tc, ENT_QUOTES));
+    
+
 }
 ?>
 
@@ -30,6 +76,6 @@ if(isset($_POST['searchterm'])) {
     Comments: <textarea name="comments"></textarea><br>
     <input type="checkbox" name ="tc" value="ok">
     I accept the T&amp;C<br>
-    <input type="submit" name="Search"><br>
+    <input type="submit" name="submit" value="Register"><br>
 
 </form>
