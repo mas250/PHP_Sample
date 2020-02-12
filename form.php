17fa5902e6c1cca7de653fab336217ae6acde7c3
mas250 <mas250@exeter.ac.uk>
2020-02-12 12:42:31 +0000

    server side form validation

--- form.php
+++ form.php
@@ -8,44 +8,69 @@ $comments = '';
 $tc = '';
 
 if(isset($_POST['submit'])) {
-    
-    if(isset($_POST['name'])){
-        $name = $_POST['name'];
-    }
-    if(isset($_POST['$password'])){
-        $password = $_POST['$password'];
-    }
-    if(isset($_POST['gender'])){
-        $gender = $_POST['gender'];
-    }
-    if(isset($_POST['color'])){
-        $color = $_POST['color'];
-    }
-    if(isset($_POST['languages'])){
-        $languages = $_POST['languages'];
-    }
-    if(isset($_POST['comments'])){
-        $comments = $_POST['comments'];
-    }
-    if(isset($_POST['tc'])){
-        $tc = $_POST['tc'];
-    }
+    $ok = true ;
 
-    printf('User name : %s
-    <br>Password: %s
-    <br>Gender: %s
-    <br>Color: %s
-    <br>Languages: %s   
-    <br>Comments: %s
-    <br>T&amp;c: %s',
-    htmlspecialchars($names, ENT_QUOTES),       //better practice to use placeholders and
-    htmlspecialchars($password, ENT_QUOTES),    //escape special characters so users cant run malicious code
-    htmlspecialchars($gender, ENT_QUOTES),           
-    htmlspecialchars($color, ENT_QUOTES),           
-    htmlspecialchars(implode(' ', $languages), ENT_QUOTES),
-    htmlspecialchars($comments, ENT_QUOTES),
-    htmlspecialchars($tc, ENT_QUOTES));
-    
+    if(!isset($_POST['name']) || $_POST['name'] === ''){
+        $ok = false;
+    }
+    else{
+        $name = $_POST['name'];   
+    };
+
+    if(!isset($_POST['password']) || $_POST['password'] === ''){
+        $ok = false;
+    }
+    else{
+        $password = $_POST['password'];
+    };
+    if(!isset($_POST['gender']) || $_POST['gender'] === ''){
+        $ok = false;
+    }
+    else{
+        $gender = $_POST['gender'];
+    };
+    if(!isset($_POST['color'])|| $_POST['color'] === ''){
+        $ok = false;
+    }
+    else{
+        $color = $_POST['color'];
+    };
+    if(!isset($_POST['languages']) || !is_array( $_POST['languages']) 
+        || count($_POST['languages']) === 0){
+        $ok = false;
+    }
+    else{
+        $languages = $_POST['languages'];
+    };
+    if(!isset($_POST['comments']) || $_POST['comments'] === ''){
+        $ok = false;
+    }
+    else{
+        $comments = $_POST['comments'];
+    };
+    if(!isset($_POST['tc']) || $_POST['name'] === ''){
+        $ok = false;
+    }
+    else{
+        $tc = $_POST['tc'];
+    };
+
+    if($ok){
+        printf('User name : %s
+        <br>Password: %s
+        <br>Gender: %s
+        <br>Color: %s
+        <br>Languages: %s   
+        <br>Comments: %s
+        <br>T&amp;c: %s',
+        htmlspecialchars($name, ENT_QUOTES),       //better practice to use placeholders and
+        htmlspecialchars($password, ENT_QUOTES),    //escape special characters so users cant run malicious code
+        htmlspecialchars($gender, ENT_QUOTES),           
+        htmlspecialchars($color, ENT_QUOTES),           
+        htmlspecialchars(implode(' ', $languages), ENT_QUOTES),
+        htmlspecialchars($comments, ENT_QUOTES),
+        htmlspecialchars($tc, ENT_QUOTES));
+    }
 
 }
 ?>
@@ -55,7 +80,7 @@ if(isset($_POST['submit'])) {
     method="post"
     >
     User name: <input type="text" name="name"><br>
-    Password: <input type="password" value="password"><br>
+    Password: <input type="password" name="password"><br>
     Gender:
         <input type="radio" name ="gender" value="f"> Female
         <input type="radio" name ="gender" value="m"> Male
